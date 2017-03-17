4ece0972c667bb8b26a75ac6093bb6cfa13162a7
Bradley Awodu <awodubradley@gmail.com>
2017-03-17 15:10:30 -0500

    - Updated web/collect_browsing_data.js

--- web/collect_browsing_data.js
+++ web/collect_browsing_data.js
@@ -26,23 +26,24 @@ console.log(wsuri);
 connection.onopen = function (session) {
    var dl = [];
 
-   dl.push(session.call('patient_assist_backend.presence_health.create_browsing_data_instance').then(
-      function (res) {
-         console.log("Result: DB ID:" + res.kwargs.id + ", Cookie ID: " + res.kwargs.cookie_id);
-      }
-   ));
-   //  var browsing_data_obj = {"cookie_id": 16,
-   //                          "keyword": "oncology",
-   //                          "keyword_clicks": 2,
-   //                          "keyword_hover_time": 3.2};
-   // dl.push(session.call('patient_assist_backend.presence_health.submit_browsing_data', [JSON.stringify(browsing_data_obj)]).then(
+   // dl.push(session.call('patient_assist_backend.presence_health.create_browsing_data_instance').then(
    //    function (res) {
-   //       console.log("Result: clicks:" + res.kwargs.oncology_clicks + ", hover time: " + res.kwargs.oncology_hover_time);
-   //    },
-   //    function (err) {
-   //       console.log("Error:", err.error, err.args, err.kwargs);
+   //       console.log("Result: DB ID:" + res.kwargs.id + ", Cookie ID: " + res.kwargs.cookie_id);
    //    }
    // ));
+
+    var browsing_data_obj = {"cookie_id": 16,
+                            "keyword": "oncology",
+                            "keyword_clicks": 2,
+                            "keyword_hover_time": 3.2};
+   dl.push(session.call('patient_assist_backend.presence_health.submit_browsing_data', [JSON.stringify(browsing_data_obj)]).then(
+      function (res) {
+         console.log("Result: clicks:" + res.kwargs.oncology_clicks + ", hover time: " + res.kwargs.oncology_hover_time);
+      },
+      function (err) {
+         console.log("Error:", err.error, err.args, err.kwargs);
+      }
+   ));
 
    when.all(dl).then(function () {
       console.log("All finished.");
