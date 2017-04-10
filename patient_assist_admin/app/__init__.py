da93a9cca059b906453c394d0d90f9f8ec653319
Bradley Awodu <awodubradley@gmail.com>
2017-04-10 16:58:33 -0500

    - Refactored patient_assist_admin component

--- patient_assist_admin/app/__init__.py
+++ patient_assist_admin/app/__init__.py
@@ -2,28 +2,35 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-import patient_assist_db_models as models
+from patient_assist_db_models import BROWSING_KEYWORDS
+from patient_assist_db_models import BROWSING_DATA_FIELDS
+from patient_assist_db_models import PresenceBrowsingData
 
-
+# Create Flask WSGI object
 app = Flask(__name__)
 
+# Add app configs to WSGI object
 app.config.from_object('patient_assist_admin.config')
 
+# Create flask db object for WSGI app
 db = SQLAlchemy(app)
 
+# Create admin page for Flask app
 admin = Admin(app, name='Patient Assist Admin', template_mode='bootstrap3')
 
+# Create a field list for browsing data objects: for use in admin pages
 browsing_data_column_list_base = ['cookie_id', 'send_cta_updates']
-for browsing_keyword in models.BROWSING_KEYWORDS:
-    for field, field_type in models.BROWSING_DATA_FIELDS.items():
+for browsing_keyword in BROWSING_KEYWORDS:
+    for field, field_type in BROWSING_DATA_FIELDS.items():
         browsing_data_column_list_base.append('{}_{}'.format(browsing_keyword, field))
 browsing_data_column_list_base.append("current_intent")
 
 
+# Add admin pane for each db model
 class BrowsingDataModelView(ModelView):
     can_create = False
     column_list = tuple(browsing_data_column_list_base)
-admin.add_view(BrowsingDataModelView(models.PresenceBrowsingData, db.session))
+admin.add_view(BrowsingDataModelView(PresenceBrowsingData, db.session))
 
 """
 If you are wondering why the import statement is at the end and not at the beginning of the script as it is always done,
